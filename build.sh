e69c819f0a4fc04f9a870036568ebab56f8f661d
shehryar49 <shehryarshahid49@gmail.com>
2024-02-29 00:44:20 +0500

    Build in Release mode

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh
-g++ -shared src/nuke.cpp -o nuke.so -fPIC -lfcgi -g -lpthread
+g++ -shared src/nuke.cpp -o nuke.so -fPIC -lfcgi -lpthread -DNDEBUG -O3
 sudo cp nuke.so /opt/zuko/modules
